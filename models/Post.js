0a761bace2d7fa0d870e5a7675f9719556c1d69f
michaelzz40 <61964609+michaelzz40@users.noreply.github.com>
2020-07-09 11:07:50 +0800

    Custom Post API

---
+++ models/Post.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Post = require("../../models/Post");
+const User = require("../../models/User");
 
 // @route    GET api/posts
-// @desc     Test route
+// @desc     Get all post
 // @access   Public
-router.get("/", (req, res) => res.send("Posts Route"));
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find().populate("user", ["name", "avatar"]);
+
+    res.json(posts);
+  } catch (err) {
+    return res.json({ msg: err.message });
+  }
+});
+
+// @route    POST api/posts
+// @desc     Create new form
+// @access   Private
+router.post("/", auth, async (req, res) => {
+  const { title, description } = req.body;
+
+  try {
+    let newPost = new Post({
+      user: req.user.id,
+      title,
+      description
+    });
+
+    await newPost.save();
+    res.json({ posts: newPost, msg: "Posts created" });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
