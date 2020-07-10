b4e488bd49d19c0ae51c38beeed1550f095934bf
michaelzz40 <61964609+michaelzz40@users.noreply.github.com>
2020-07-11 03:39:37 +0800

    Forms

---
+++ client/src/actions/profile.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { setAlert } from "./alert";
+
+import { GET_PROFILE, PROFILE_ERROR } from "./types";
+
+//Get current user's profile
+export const getCurrentProfile = () => async dispatch => {
+  try {
+    const res = await axios.get("/api/profile/me");
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Create or update profile
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    const res = await axios.post("/api/profile", formData, config);
+
+    dispatch(setAlert(edit ? "Profile updated" : "Profile Created", "success"));
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, Fragment } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../actions/auth";
 
-const Navbar = () => {
-  return (
-    <nav className='navbar bg-dark'>
-      <h1>
-        <Link to='/'>
-          <i className='fas fa-code'></i> DevConnector
-        </Link>
-      </h1>
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <Fragment>
+      <ul>
+        <li>
+          <Link to='/dashboard'>
+            <i className='fas fa-user'></i>{" "}
+            <span className='hide-sm'>Dashboard</span>
+          </Link>
+        </li>
+        <li>
+          <a onClick={logout} href='#!'>
+            <i className='fas fa-sign-out-alt'></i>{" "}
+            <span className='hide-sm'>Logout</span>
+          </a>
+        </li>
+      </ul>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
       <ul>
         <li>
           <Link to='profiles.html'>Developers</Link>
@@ -20,8 +37,30 @@ const Navbar = () => {
           <Link to='/login'>Login</Link>
         </li>
       </ul>
+    </Fragment>
+  );
+
+  return (
+    <nav className='navbar bg-dark'>
+      <h1>
+        <Link to='/'>
+          <i className='fas fa-code'></i> DevConnector
+        </Link>
+      </h1>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
